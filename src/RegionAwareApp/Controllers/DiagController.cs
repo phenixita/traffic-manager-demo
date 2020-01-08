9f7d6bcf7ee8b2804f64b5b62c5a8ca47ecc60c5
Michele Ferracin <miferrac@microsoft.com>
2020-01-08 14:54:51 +0100

    With diag

---
+++ src/RegionAwareApp/Controllers/DiagController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegionAwareApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DiagController : ControllerBase
+    {
+        // GET: api/Diag
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var diagConfig = Environment.GetEnvironmentVariable("DIAG") ?? "OK";
+
+            if (diagConfig.Equals("KO",StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new StatusCodeResult(503);
+            }
+
+            return "Diag OK";
+        }
+    }
+}
